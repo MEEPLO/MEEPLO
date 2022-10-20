4e4f57fe1f4d91e9c0761130700f98ddf80fe2ed
신민아 <minah741@naver.com>
2022-10-20 23:22:43 +0900

    🍸 [FEAT] INIT backend project
    - backend 프로젝트 initial 버전 생성

---
+++ Backend/src/main/java/com/sloth/momentary/MomentaryApplication.java
@@ -0,0 +1,13 @@
+package com.sloth.momentary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentaryApplication.class, args);
+	}
+
+}
