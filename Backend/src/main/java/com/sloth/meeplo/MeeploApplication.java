76407788a94c0f9a885daec7cafd9d79a5b5f92d
신민아 <minah741@naver.com>
2022-10-21 18:21:55 +0900

    🍸 [FEAT] MODIFY service name
    - 서비스 이름 Meeplo로 변경.

--- Backend/src/main/java/com/sloth/meeplo/MeeploApplication.java
+++ Backend/src/main/java/com/sloth/meeplo/MeeploApplication.java
@@ -1,13 +1,13 @@
-package com.sloth.momentary;
+package com.sloth.meeplo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MomentaryApplication {
+public class MeeploApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MomentaryApplication.class, args);
+		SpringApplication.run(MeeploApplication.class, args);
 	}
 
 }
