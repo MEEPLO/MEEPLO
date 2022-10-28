685950546ef40fc3bd0adb2d0f010e086ef99ce2
남궁준 <melon0soda@gmail.com>
2022-10-28 16:03:27 +0900

    :space_invader: [FEAT] kakao 로그인 정보 DB 저장 구현

--- Backend/src/main/java/com/sloth/meeplo/MeeploApplication.java
+++ Backend/src/main/java/com/sloth/meeplo/MeeploApplication.java
@@ -2,8 +2,10 @@ package com.sloth.meeplo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MeeploApplication {
 
 	public static void main(String[] args) {
