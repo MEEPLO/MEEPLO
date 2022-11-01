fe658275443b18a60c0355afc88ae33b80944805
rudwns06 <rudwns06@hanmail.net>
2022-11-01 18:01:47 +0900

    ⁉️ [FEAT] 모임 API 중간 커밋

--- Backend/src/main/java/com/sloth/meeplo/group/service/GroupServiceImpl.java
+++ Backend/src/main/java/com/sloth/meeplo/group/service/GroupServiceImpl.java
@@ -3,6 +3,7 @@ package com.sloth.meeplo.group.service;
 import com.sloth.meeplo.global.exception.MeeploException;
 import com.sloth.meeplo.global.exception.code.CommonErrorCode;
 import com.sloth.meeplo.global.type.Role;
+import com.sloth.meeplo.global.util.JwtUtil;
 import com.sloth.meeplo.group.dto.request.GroupRequest;
 import com.sloth.meeplo.group.dto.response.GroupResponse;
 import com.sloth.meeplo.group.entity.Group;
@@ -11,6 +12,7 @@ import com.sloth.meeplo.group.repository.GroupMemberRepository;
 import com.sloth.meeplo.group.repository.GroupRepository;
 import com.sloth.meeplo.group.type.GroupMemberStatus;
 import com.sloth.meeplo.member.entity.Member;
+import com.sloth.meeplo.member.repository.MemberRepository;
 import com.sloth.meeplo.schedule.entity.Schedule;
 import com.sloth.meeplo.schedule.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +23,6 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -32,6 +33,9 @@ public class GroupServiceImpl implements GroupService{
     private final GroupMemberRepository groupMemberRepository;
     private final ScheduleRepository scheduleRepository;
 
+    private final JwtUtil jwtUtil;
+    private final MemberRepository memberRepository;
+
 
     private final LocalDate date = LocalDate.of(1111, 11,  11);
     private final LocalTime time = LocalTime.of(11,11,11);
@@ -39,7 +43,6 @@ public class GroupServiceImpl implements GroupService{
     public Long makeGroup(String authorization, GroupRequest.GroupInput groupInput) {
         Group group = groupRepository.save(groupInput.toEntity());
         Member member = null;
-        // TODO: 2022-10-31 jwt token 인식 이후 member데이터 접근 추가
         joinGroup(group, member, Role.LEADER);
         return group.getId();
     }
@@ -47,18 +50,19 @@ public class GroupServiceImpl implements GroupService{
     @Override
     public void updateGroup(String authorization, Long groupId, GroupRequest.GroupInput groupInput) {
         Group group = getGroupEntityByGroupId(groupId);
-        Member member;
-
-
+        Member member = getMemberByAuthorization(authorization);
+// TODO: 2022-11-01 ID만 바꿔서 가능한지 확인필요
         if(isGroupLeader(group, member)){
-            group = groupRepository.save(groupInput.toEntity());
+            group = groupInput.toEntity();
+            group.updateGroupId(groupId);
+            groupRepository.save(group);
         }
     }
 
     @Override
     public void deleteGroup(String authorization, Long groupId) {
         Group group = getGroupEntityByGroupId(groupId);
-        Member member;
+        Member member = getMemberByAuthorization(authorization);
         if(isGroupLeader(group, member)){
             groupRepository.delete(group);
         }
@@ -66,7 +70,7 @@ public class GroupServiceImpl implements GroupService{
 
     @Override
     public List<GroupResponse.JoinedGroupSummary> joinedGroupList(String authorization) {
-        Member member;
+        Member member = getMemberByAuthorization(authorization);
 
         List<GroupMember> groupMemberList = groupMemberRepository.findByMemberAndStatus(member, GroupMemberStatus.ACTIVATED);
 
@@ -96,7 +100,6 @@ public class GroupServiceImpl implements GroupService{
 
     @Override
     public GroupResponse.JoinedGroupDetail getJoinedGroupDetail(String authorization, Long groupId) {
-        Member member;
         Group group = getGroupEntityByGroupId(groupId);
         List<GroupMember> groupMembers = groupMemberRepository.findByGroupAndStatus(group, GroupMemberStatus.ACTIVATED);
         GroupMember leader = groupMemberRepository.findByGroupAndRoleAndStatus(group, Role.LEADER, GroupMemberStatus.ACTIVATED)
@@ -133,7 +136,7 @@ public class GroupServiceImpl implements GroupService{
 
     @Override
     public void exitGroupMember(String authorization, Long groupId) {
-        Member member;
+        Member member = getMemberByAuthorization(authorization);
         Group group = getGroupEntityByGroupId(groupId);
         GroupMember groupMember= groupMemberRepository.findByGroupAndMember(group, member)
                 .orElseThrow(()-> new MeeploException(CommonErrorCode.NOT_EXIST_RESOURCE));
@@ -142,6 +145,12 @@ public class GroupServiceImpl implements GroupService{
 
     private Group getGroupEntityByGroupId(Long groupId){
         return groupRepository.findById(groupId)
+                .orElseThrow(()-> new MeeploException(CommonErrorCode.NOT_EXIST_RESOURCE));
+    }
+
+    private Member getMemberByAuthorization(String authorization){
+
+        return memberRepository.findById(jwtUtil.getUserIdFromToken(authorization))
                 .orElseThrow(()-> new MeeploException(CommonErrorCode.NOT_EXIST_RESOURCE));
     }
 
