0c78326bd87dcb050a95e6a0d9836cd18aa2f2d8
남궁준 <melon0soda@gmail.com>
2022-11-01 15:52:20 +0900

    :space_invader: [FEAT] JWT added - JWT 발급을 위한 url /kakao/login 을 /auth/kakao 로 변경 - 해당 요청 제외한 모든 요청에 JWT 검증을 위한 Filter 추가

--- Backend/src/main/java/com/sloth/meeplo/global/util/JwtUtil.java
+++ Backend/src/main/java/com/sloth/meeplo/global/util/JwtUtil.java
@@ -1,14 +1,18 @@
 package com.sloth.meeplo.global.util;
 
+import com.sloth.meeplo.global.exception.MeeploException;
+import com.sloth.meeplo.global.exception.code.CommonErrorCode;
 import com.sloth.meeplo.global.type.TokenType;
 import com.sloth.meeplo.member.entity.Member;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 @Component
@@ -39,9 +43,24 @@ public class JwtUtil {
         try {
             return Long.parseLong(Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject());
         } catch(Exception e) {
-            e.printStackTrace();
+            throw new MeeploException(CommonErrorCode.WRONG_TOKEN);
         }
+    }
 
-        return -1L;
+    public String resolveToken(HttpServletRequest request) {
+        try {
+            return request.getHeader("Authorization").replaceFirst("Bearer ", "");
+        } catch(Exception e) {
+            throw new MeeploException(CommonErrorCode.WRONG_TOKEN);
+        }
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch(Exception e) {
+            throw new MeeploException(CommonErrorCode.WRONG_TOKEN);
+        }
     }
 }
--- Backend/src/main/java/com/sloth/meeplo/member/controller/MemberController.java
+++ Backend/src/main/java/com/sloth/meeplo/member/controller/MemberController.java
@@ -5,12 +5,14 @@ import com.sloth.meeplo.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("meeplo/api/v1")
 public class MemberController {
 
     private final MemberService memberService;
@@ -19,7 +21,7 @@ public class MemberController {
      * @param authorization 카카오에서 발급한 토큰
      * @return : accessToken, refreshToken, 신규 회원 여부
      */
-    @GetMapping("/kakao/token")
+    @GetMapping("/auth/kakao")
     public ResponseEntity<MemberResponse.MemberToken> getAppTokenForKakaoLogin(@RequestHeader("Authorization") String authorization) {
         MemberResponse.MemberToken memberToken = memberService.getKakaoMemberToken(authorization);
 
