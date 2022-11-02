ba7c429cf76f657e1533caa6171ba3f1fd6854ea
jekwan <jekwan@kakao.com>
2022-11-03 00:47:56 +0900

    🐣 [FEAT] update calendar style

--- frontend/meeplo/src/config/config.calender.js
+++ frontend/meeplo/src/config/config.calender.js
@@ -2,6 +2,8 @@ import React from 'react';
 import CalendarHeader from '../components/calendar/CalendarHeader';
 import { LocaleConfig } from 'react-native-calendars';
 
+const today = new Date();
+
 // global local settings
 LocaleConfig.locales['ko'] = {
   monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
@@ -10,18 +12,53 @@ LocaleConfig.locales['ko'] = {
   dayNamesShort: ['월', '화', '수', '목', '금', '토', '일'],
   today: '오늘',
 };
-LocaleConfig.defaultLocale = 'ko';
+// LocaleConfig.defaultLocale = 'ko';
 
 const calendar = {};
-const today = new Date();
+
 calendar.meeploCalendarParam = {
   renderHeader: date => {
     return <CalendarHeader date={date} />;
   },
 
   enableSwipeMonths: true,
-  minDate: '2020-10-01',
-  maxDate: `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate() - 1}`,
+  // minDate: '2020-10-01',
+  // maxDate: `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate() - 1}`,
+};
+
+calendar.meeploCalendarParamTheme = {
+  backgroundColor: '#ffffff',
+  calendarBackground: '#ffffff',
+  textSectionTitleColor: '#b6c1cd',
+  textSectionTitleDisabledColor: '#ffffff',
+  selectedDayBackgroundColor: '#00adf5',
+  selectedDayTextColor: '#ffffff',
+  todayTextColor: '#00adf5',
+  dayTextColor: '#2d4150',
+  textDisabledColor: '#d9e1e8',
+  dotColor: '#00adf5',
+  selectedDotColor: '#ffffff',
+  arrowColor: 'orange',
+  disabledArrowColor: '#d9e1e8',
+  monthTextColor: 'blue',
+  indicatorColor: 'blue',
+  textDayFontFamily: 'monospace',
+  textMonthFontFamily: 'monospace',
+  textDayHeaderFontFamily: 'monospace',
+  textDayFontWeight: '300',
+  textMonthFontWeight: 'bold',
+  textDayHeaderFontWeight: '300',
+  textDayFontSize: 16,
+  textMonthFontSize: 16,
+  textDayHeaderFontSize: 16,
+  'stylesheet.calendar.header': {
+    dayTextAtIndex0: {
+      color: 'blue',
+    },
+    dayTextAtIndex6: {
+      color: 'blue',
+    },
+  },
 };
 
 export default calendar;
