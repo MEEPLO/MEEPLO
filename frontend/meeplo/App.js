4da29d2612404aa9ae7e899c16981dd1eb070189
김제관 <jekwan@kakao.com>
2022-11-02 13:53:58 +0900

    🐣 [FEAT] add navigation feature and create some header components

--- frontend/meeplo/App.js
+++ frontend/meeplo/App.js
@@ -1,37 +1,55 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-import {Provider} from 'react-redux';
-import {store} from './src/redux/store';
+import { SafeAreaView, StatusBar, Text, useColorScheme, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HeaderTitle from './src/components/header/HeaderTitle';
+import HeaderRight from './src/components/header/HeaderRight';
+
+import Home from './src/screens/Home';
+import Details from './src/screens/Details';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const headerSytle = {
+    headerStyle: {
+      backgroundColor: '#fff',
+    },
+    headerTintColor: 'red',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+    headerTitleAlign: 'center',
   };
 
   return (
     <NavigationContainer>
       <Provider store={store}>
-        <SafeAreaView style={backgroundStyle}>
-          <StatusBar
-            barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-            backgroundColor={backgroundStyle.backgroundColor}
-          />
-          <View style={backgroundStyle}>
-            <Text>Hello Meeplo app!</Text>
-          </View>
-        </SafeAreaView>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <Stack.Navigator
+          initialRouteName="home"
+          screenOptions={{
+            ...headerSytle,
+            headerTitle: () => <HeaderTitle />,
+            headerRight: () => <HeaderRight />,
+          }}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Details" component={Details} />
+        </Stack.Navigator>
       </Provider>
     </NavigationContainer>
   );
