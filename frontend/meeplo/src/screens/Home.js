ed3ecb387a9e17427d0868ddeb9fc90e3f8108db
김제관 <jekwan@kakao.com>
2022-11-02 18:02:31 +0900

    🐣 [FEAT] test code for schedule home comp

---
+++ frontend/meeplo/src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const Home = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
+    </View>
+  );
+};
+
+export default Home;
