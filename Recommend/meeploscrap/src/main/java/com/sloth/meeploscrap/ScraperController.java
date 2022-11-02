c2b9ad895787dfef8ff2c3b2363addfc701e4690
신민아 <minah741@naver.com>
2022-11-02 18:01:49 +0900

    🍸 [FEAT] CONNECT selenium with Chrome Driver
    - 셀레니움 초기 코드 완성.

---
+++ Recommend/meeploscrap/src/main/java/com/sloth/meeploscrap/ScraperController.java
@@ -0,0 +1,17 @@
+package com.sloth.meeploscrap;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ScraperController {
+    private final ScraperService scraperService;
+
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok().body(scraperService.scrapDataFromWeb());
+    }
+}
