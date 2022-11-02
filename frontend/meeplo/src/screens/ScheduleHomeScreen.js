a26f434a8a341878803ab2e03178629718fe134a
김제관 <jekwan@kakao.com>
2022-11-02 18:01:32 +0900

    🐣 [FEAT] add Schedule Home Component

---
+++ frontend/meeplo/src/screens/ScheduleHomeScreen.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+import { Calendar } from 'react-native-calendars';
+import config from '../config';
+
+const TitleView = styled.View`
+  border-width: 1px;
+  border-color: black;
+`;
+
+const StyledText = styled.Text`
+  color: #000;
+  font-size: 20px;
+  margin: 10px;
+  padding: 10px;
+`;
+
+const ScheduleHomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <TitleView>
+        <StyledText>약속 리스트</StyledText>
+      </TitleView>
+      <View style={{ borderWidth: 4, borderColor: 'black' }}>
+        <Calendar
+          {...config.calendar.meeploCalendarParam}
+          onDayPress={day => console.log(day)}
+          onDayLongPress={day => console.log('long', day)}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ScheduleHomeScreen;
