f27d9da14b4749c3382e1f1f373eca098803e2d1
jekwan <jekwan@kakao.com>
2022-11-03 00:47:56 +0900

    🐣 [FEAT] update calendar style

--- frontend/meeplo/src/components/calendar/CalendarHeader.js
+++ frontend/meeplo/src/components/calendar/CalendarHeader.js
@@ -1,17 +1,41 @@
-import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import React, { useMemo, useState } from 'react';
+import styled from 'styled-components/native';
+
+const monthText = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const HeaderView = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: black;
+  border-radius: 5px;
+`;
+
+const MonthLabel = styled.Text`
+  padding: 2px 8px 2px 8px;
+  font-weight: 400;
+  color: white;
+`;
 
 const CalendarHeader = ({ date }) => {
-  const [format, setFormat] = useState('');
-
-  if (typeof date === 'object') {
-  }
-
-  console.log(date);
   return (
-    <View>
-      <Text>wow</Text>
-    </View>
+    <HeaderView>
+      <MonthLabel>{monthText[new Date(date).getMonth()]}</MonthLabel>
+    </HeaderView>
   );
 };
 
