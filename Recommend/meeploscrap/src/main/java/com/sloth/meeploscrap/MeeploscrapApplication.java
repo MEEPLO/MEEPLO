38d648ca0464dae27305d64f2e032906f675843d
신민아 <minah741@naver.com>
2022-11-02 16:05:37 +0900

    🍸 [FEAT] INIT web scraper from Spring
    - Spring으로 selenium을 이용하기 위한 초기 설정 완료.

---
+++ Recommend/meeploscrap/src/main/java/com/sloth/meeploscrap/MeeploscrapApplication.java
@@ -0,0 +1,13 @@
+package com.sloth.meeploscrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeeploscrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeeploscrapApplication.class, args);
+	}
+
+}
