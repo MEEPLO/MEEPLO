92534b9aafe9758538f165d881180a0d9218fffb
김혜림 <bljh1008@naver.com>
2022-11-02 17:53:40 +0900

    :cherry_blossom: [FEAT] add Splash, Login screens

---
+++ frontend/meeplo/src/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
+// import styled from 'styled-components';
+
+const HomeScreen = () => {
+  return (
+    <>
+      <View>
+        <Text>여기는 예정된 약속</Text>
+      </View>
+      <View>
+        <Text>여기는 내가 속한 그룹</Text>
+      </View>
+      <View>
+        <Text>여기는 기록 남길 약속</Text>
+      </View>
+      <View>
+        <Text>여기는 추천</Text>
+      </View>
+    </>
+  );
+};
+
+export default HomeScreen;
---
+++ frontend/meeplo/src/screens/SplashScreen.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { StatusBar, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import styled from 'styled-components';
+
+const SplashView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #465bd8;
+`;
+
+const SplashLogo = styled.Text`
+  font-size: 50px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.color.background};
+`;
+
+const SplashScreen = ({ navigation }) => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  // TODO: login 된 사용자면 바로 Home으로 넘김
+  // TODO: login 되지 않은 사용자면 LoginScreen으로 넘김
+  useEffect(() => {
+    const getIsLogin = () => {
+      // if ((await AsyncStorage.getItem('accessToken')) !== null) {
+      setIsLogin(true);
+      // }
+    };
+    getIsLogin();
+
+    setTimeout(() => {
+      if (isLogin === true) {
+        navigation.replace('Home');
+      } else {
+        // navigation.replace('Home');
+        navigation.replace('Login');
+      }
+    }, 2500);
+  }, []);
+
+  return (
+    <SplashView>
+      <StatusBar animated={true} barStyle="light-content" hidden={true} backgroundColor="#465bd8" />
+      <Image source={require('../assets/image/test_loopy.png')} style={{ width: 100, height: 100 }} />
+      <SplashLogo>MEEPLO</SplashLogo>
+    </SplashView>
+  );
+};
+
+export default SplashScreen;
