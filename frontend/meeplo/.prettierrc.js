2966642b2ed1a29200b4424bc4eca91dd5e4b492
김제관 <jekwan@kakao.com>
2022-11-02 11:00:19 +0900

    🐣 [UPDATE] prettier configuration

--- frontend/meeplo/.prettierrc.js
+++ frontend/meeplo/.prettierrc.js
@@ -1,7 +1,20 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
-};
+  tabWidth: 2,
+  jsxBracketSameline: true,
+  printWidth: 120,
+  useTabs: false,
+  semi: true,
+  overrides: [
+    {
+      files: '*.json',
+      options: {
+        printWidth: 200,
+      },
+    },
+  ],
+}
