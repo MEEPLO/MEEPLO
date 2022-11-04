81188a2339be0def84a896f3a35033e686355209
김혜림 <bljh1008@naver.com>
2022-11-04 17:58:31 +0900

    :cherry_blossom: [UPDATE]  background to white

--- frontend/meeplo/App.js
+++ frontend/meeplo/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { ThemeProvider } from 'styled-components';
 import NavigationBar from './src/components/common/NavigationBar';
 import { theme } from './src/assets/constant/DesignTheme';
 
 const App = () => {
+  const navTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: '#FFFFFF',
+    },
+  };
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={navTheme}>
       <Provider store={store}>
         <ThemeProvider theme={theme}>
           {/* 스플래휘 화면이랑 로그인 화면은 여기에 있어야 하지 않을까
