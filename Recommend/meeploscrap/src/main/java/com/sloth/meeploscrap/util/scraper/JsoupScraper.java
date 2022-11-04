7b1ebce7a511b40df22b6f9ff6102f4a9e380913
신민아 <minah741@naver.com>
2022-11-04 17:45:50 +0900

    🍸 [FEAT] CAPTURE detail data
    - 상세 정보 모두 가져오는 로직 구현.

--- Recommend/meeploscrap/src/main/java/com/sloth/meeploscrap/util/scraper/JsoupScraper.java
+++ Recommend/meeploscrap/src/main/java/com/sloth/meeploscrap/util/scraper/JsoupScraper.java
@@ -6,7 +6,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -18,6 +21,20 @@ public class JsoupScraper implements Scraper{
 
         Elements elements = parsedHTML.select(DETAIL_DATA_CSS_SELECTOR + "li.SF_Mq.SjF5j > div > span.dry01");
 
+        Elements photos = parsedHTML.select(".K0PDV");
+
+        log.info(photos.stream().map(p ->  p.attr("style"))
+                .map(s -> Arrays.stream(s.split(";"))
+                            .filter(element -> element.startsWith("background-image"))
+                            .map(b -> b.replace("background-image:url(", "").replace(")", ""))
+                            .findFirst().orElse(null))
+                .filter(url -> url != null)
+                .collect(Collectors.toList()).toString());
+
+        Elements keywords = parsedHTML.select(".sJgQj");
+
+        log.info(keywords.stream().map(k -> k.text().replace(", ", "")).collect(Collectors.toList()).toString());
+
         if(!elements.isEmpty()) {
             log.info(elements.first().text());
         }
@@ -28,7 +45,9 @@ public class JsoupScraper implements Scraper{
                 Map.entry("phoneNumber", "li.SF_Mq.SjF5j > div > span.dry01"),
                 Map.entry("simpleDescription", "li.SF_Mq.I5Ypx > div > div > span"),
                 Map.entry("detailDescription", "li.SF_Mq.I5Ypx > div > a > span.zPfVt"),
-                Map.entry("link", "li.SF_Mq.nKpE4 > div > div > a")
+                Map.entry("link", "li.SF_Mq.nKpE4 > div > div > a"),
+                Map.entry("photos", ".K0PDV"),
+                Map.entry("keywords", ".sJgQj")
         );
     }
 
