998d481a201d8cf80c13e96b909d07cf5d68157a
김혜림 <bljh1008@naver.com>
2022-11-04 14:09:19 +0900

    :cherry_blossom: [FEAT] add ToolBar

---
+++ frontend/meeplo/src/components/common/ToolBar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons/faChevronLeft';
+import { theme } from '../../assets/constant/DesignTheme';
+
+const ToolBarTitle = () => {
+  return (
+    <View style={{ backgroundColor: theme.color.background, height: 50, justifyContent: 'center' }}>
+      <Image style={{ height: 30 }} source={require('../../assets/image/meeploLogo.png')} resizeMode="contain" />
+    </View>
+  );
+};
+
+const ToolBarLeft = props => {
+  console.log(props);
+  const navigation = useNavigation();
+  return (
+    <TouchableOpacity
+      style={{ alignItems: 'center', justifyContent: 'center' }}
+      onPress={() => {
+        navigation.goBack();
+      }}>
+      <FontAwesomeIcon icon={faChevronLeft} size={25} color="#959595" />
+    </TouchableOpacity>
+  );
+};
+
+const ToolBarRight = ({ userPhoto }) => {
+  return (
+    <View style={{ height: 50, alignItems: 'center', justifyContent: 'center' }}>
+      <Image style={{ width: 40, height: 40, borderRadius: 20 }} source={{ uri: userPhoto }} resizeMode="contain" />
+    </View>
+  );
+};
+
+export { ToolBarLeft, ToolBarTitle, ToolBarRight };
--- frontend/meeplo/src/screens/HomeStackScreen.js
+++ frontend/meeplo/src/screens/HomeStackScreen.js
@@ -1,25 +1,56 @@
 import React, { useEffect } from 'react';
-import { Text } from 'react-native-svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { Text, Button, View } from 'react-native-svg';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from './SplashScreen';
 import LoginScreen from './LoginScreen';
 import HomeScreen from './HomeScreen';
 import LoadingBar from '../components/common/LoadingBar';
-// import styled from 'styled-components';
+import { ToolBarLeft, ToolBarRight, ToolBarTitle } from '../components/common/ToolBar';
+import { getUserInfo } from '../redux/userSlice';
+import HomeGroup from '../components/Home/HomeGroup';
 
 const HomeStack = createNativeStackNavigator();
 
 const HomeStackScreen = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.user);
+
+  useEffect(() => {
+    dispatch(getUserInfo());
+  }, []);
   return (
-    <HomeStack.Navigator
-      initialRouteName="Splash"
-      screenOptions={{
-        headerShown: false,
-      }}>
-      <HomeStack.Screen name="Splash" component={SplashScreen} />
-      <HomeStack.Screen name="Home" component={HomeScreen} />
-      <HomeStack.Screen name="Login" component={LoginScreen} />
-      <HomeStack.Screen name="Loading" component={LoadingBar} />
+    <HomeStack.Navigator initialRouteName="Splash">
+      <HomeStack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: true,
+          headerTitleAlign: 'center',
+          headerLeft: props => {
+            props.canGoBack && <ToolBarLeft {...props} />;
+          },
+          headerRight: () => <ToolBarRight userPhoto={user.memberDetail.profilePhoto} />,
+          headerTitle: () => <ToolBarTitle />,
+        }}
+      />
+      <HomeStack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+      <HomeStack.Screen name="Loading" component={LoadingBar} options={{ headerShown: false }} />
+      <HomeStack.Screen
+        name="HomeGroup"
+        component={HomeGroup}
+        options={{
+          headerShown: true,
+          headerTitleAlign: 'center',
+          headerBackVisible: false,
+          headerLeft: props => {
+            return props.canGoBack && <ToolBarLeft {...props} />;
+          },
+          headerRight: () => <ToolBarRight userPhoto={user.memberDetail.profilePhoto} />,
+          headerTitle: () => <ToolBarTitle />,
+        }}
+      />
     </HomeStack.Navigator>
   );
 };
