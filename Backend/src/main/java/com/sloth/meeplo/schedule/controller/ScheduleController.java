f051c297f0b38d8eb63c7063b256a43d62095b3c
rudwns06 <rudwns06@hanmail.net>
2022-11-06 16:13:06 +0900

    ⁉️ [FEAT] Schedule 조회 API 작성

--- Backend/src/main/java/com/sloth/meeplo/schedule/controller/ScheduleController.java
+++ Backend/src/main/java/com/sloth/meeplo/schedule/controller/ScheduleController.java
@@ -2,6 +2,7 @@ package com.sloth.meeplo.schedule.controller;
 
 
 import com.sloth.meeplo.schedule.dto.request.ScheduleRequest;
+import com.sloth.meeplo.schedule.dto.response.ScheduleResponse;
 import com.sloth.meeplo.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -30,5 +32,23 @@ public class ScheduleController {
     public ResponseEntity<Void> updateSchedule(@RequestHeader("Authorization") String authorization, @RequestBody ScheduleRequest.ScheduleUpdateInput scheduleUpdateInput){
         scheduleService.updateSchedule(authorization, scheduleUpdateInput);
         return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{scheduleId}")
+    public ResponseEntity<Void> deleteSchedule(@RequestHeader("Authorization") String authorization, @PathVariable Long scheduleId){
+        scheduleService.deleteSchedule(authorization, scheduleId);
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{scheduleId}")
+    public ResponseEntity<ScheduleResponse.ScheduleDetailInfo> getScheduleDetail(@RequestHeader("Authorization") String authorization, @PathVariable Long scheduleId){
+        return new ResponseEntity<>(scheduleService.getScheduleDetail(authorization,scheduleId), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Map<String, List<ScheduleResponse.ScheduleListInfo>>> getScheduleList(@RequestHeader("Authorization") String authorization){
+        Map<String, List<ScheduleResponse.ScheduleListInfo>> resultMap = new HashMap<>();
+        resultMap.put("schedules", scheduleService.getScheduleList(authorization));
+        return new ResponseEntity<>(resultMap, HttpStatus.OK);
     }
 }
--- Backend/src/main/java/com/sloth/meeplo/schedule/dto/response/ScheduleResponse.java
+++ Backend/src/main/java/com/sloth/meeplo/schedule/dto/response/ScheduleResponse.java
@@ -1,4 +1,172 @@
 package com.sloth.meeplo.schedule.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sloth.meeplo.group.entity.Group;
+import com.sloth.meeplo.location.entity.Location;
+import com.sloth.meeplo.schedule.entity.Schedule;
+import com.sloth.meeplo.schedule.entity.ScheduleKeyword;
+import com.sloth.meeplo.schedule.entity.ScheduleLocation;
+import com.sloth.meeplo.schedule.entity.ScheduleMember;
+import com.sloth.meeplo.schedule.type.ScheduleMemberStatus;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ScheduleResponse {
+    @Getter
+    @ToString
+    @NoArgsConstructor
+    public static class ScheduleDetailInfo{
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+        private LocalDateTime date;
+        private String name;
+        private ScheduleDetailGroupInfo group;
+        private List<ScheduleDetailKeywordInfo> keywords;
+        private List<ScheduleDetailMemberInfo> members;
+        private ScheduleDetailMeetLocationInfo meetLocation;
+        private List<ScheduleDetailAmuseLocationInfo> amuseLocations;
+
+        @Builder
+        ScheduleDetailInfo(Schedule schedule){
+            this.date = schedule.getDate();
+            this.name = schedule.getName();
+            this.keywords = schedule.getScheduleKeywords().stream().map(kw -> ScheduleDetailKeywordInfo.builder().keyword(kw).build()).collect(Collectors.toList());
+            this.group = ScheduleDetailGroupInfo.builder().group(schedule.getGroup()).build();
+            this.members = schedule.getScheduleMembers().stream().map(sm -> ScheduleDetailMemberInfo.builder().scheduleMember(sm).build()).collect(Collectors.toList());
+            this.meetLocation = ScheduleDetailMeetLocationInfo.builder().location(schedule.getLocation()).build();
+            this.amuseLocations = schedule.getScheduleLocations().stream().map(sl -> ScheduleDetailAmuseLocationInfo.builder().scheduleLocation(sl).build()).collect(Collectors.toList());
+
+        }
+    }
+    @Getter
+    @ToString
+    @NoArgsConstructor
+    public static class ScheduleDetailGroupInfo{
+        private Long id;
+        private String name;
+
+        @Builder
+        ScheduleDetailGroupInfo(Group group){
+            this.id = group.getId();
+            this.name = group.getName();
+        }
+    }
+
+    @Getter
+    @ToString
+    @NoArgsConstructor
+    public static class ScheduleDetailKeywordInfo{
+        private Long id;
+        private String content;
+
+        @Builder
+        ScheduleDetailKeywordInfo(ScheduleKeyword keyword){
+            this.id = keyword.getId();
+            this.content = keyword.getKeyword();
+        }
+
+    }
+
+    @Getter
+    @ToString
+    @NoArgsConstructor
+    public static class ScheduleDetailMemberInfo{
+        private Long id;
+        private String nickname;
+        private String photo;
+        private ScheduleMemberStatus status;
+
+        @Builder
+        ScheduleDetailMemberInfo(ScheduleMember scheduleMember){
+            this.id = scheduleMember.getId();
+            this.nickname = scheduleMember.getMember().getGroupMembers().stream().filter(gm -> gm.getMember().equals(scheduleMember.getMember())).map(gm -> gm.getNickname()).collect(Collectors.toList()).get(0);
+            this.photo = scheduleMember.getMember().getProfilePhoto();
+            this.status = scheduleMember.getStatus();
+        }
+    }
+
+    @Getter
+    @ToString
+    @NoArgsConstructor
+    public static class ScheduleDetailMeetLocationInfo{
+        private Long id;
+        private String name;
+        private String address;
+        private Double lat;
+        private Double lng;
+
+        @Builder
+        ScheduleDetailMeetLocationInfo(Location location){
+            this.id = location.getId();
+            this.name = location.getName();
+            this.address = location.getAddress();
+            this.lat = location.getLat();
+            this.lng = location.getLng();
+        }
+    }
+
+    @Getter
+    @ToString
+    @NoArgsConstructor
+    public static class ScheduleDetailAmuseLocationInfo{
+        private Long id;
+        private String name;
+        private String address;
+        private Double lat;
+        private Double lng;
+
+        @Builder
+        ScheduleDetailAmuseLocationInfo(ScheduleLocation scheduleLocation){
+            this.id = scheduleLocation.getId();
+            this.name = scheduleLocation.getLocation().getName();
+            this.address = scheduleLocation.getLocation().getAddress();
+            this.lat = scheduleLocation.getLocation().getLat();
+            this.lng = scheduleLocation.getLocation().getLng();
+        }
+    }
+
+    @Getter
+    @ToString
+    @NoArgsConstructor
+    public static class ScheduleListInfo{
+        private Long id;
+        private String name;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+        private LocalDateTime date;
+        private String groupName;
+        private Long memberCount;
+        private Boolean momentRecorded;
+        private ScheduleListLocationInfo location;
+
+        @Builder
+        ScheduleListInfo(Schedule schedule){
+            this.id = schedule.getId();
+            this.name = schedule.getName();
+            this.date = schedule.getDate();
+            this.groupName = schedule.getGroup().getName();
+            this.memberCount = schedule.getScheduleMembers().stream().filter(sm -> sm.getStatus().equals(ScheduleMemberStatus.JOINED)).count();
+            this.momentRecorded = schedule.getScheduleLocations().stream().map(sl -> (long) sl.getMoments().size()).anyMatch(c -> c > 0);
+            this.location = ScheduleListLocationInfo.builder().schedule(schedule).build();
+        }
+
+    }
+    @Getter
+    @ToString
+    @NoArgsConstructor
+    public static class ScheduleListLocationInfo{
+        private String meetName;
+        private String amuseName;
+
+        @Builder
+        ScheduleListLocationInfo(Schedule schedule){
+            this.meetName = schedule.getLocation().getName();
+            this.amuseName = schedule.getScheduleLocations().stream().findFirst().orElse(ScheduleLocation.EmptyScheduleLocation().location(Location.builder().name("미정").build()).build()).getLocation().getName();
+        }
+    }
 }
