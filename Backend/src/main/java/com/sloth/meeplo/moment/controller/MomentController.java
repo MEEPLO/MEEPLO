723bb04739e400a398d341e2252b8875ac2311a7
rudwns06 <rudwns06@hanmail.net>
2022-11-06 23:48:39 +0900

    ⁉️ [FEAT]추억상세보기 api

---
+++ Backend/src/main/java/com/sloth/meeplo/moment/controller/MomentController.java
@@ -0,0 +1,32 @@
+package com.sloth.meeplo.moment.controller;
+
+import com.sloth.meeplo.moment.dto.request.MomentRequest;
+import com.sloth.meeplo.moment.dto.response.MomentResponse;
+import com.sloth.meeplo.moment.service.MomentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("meeplo/api/v1/moment")
+public class MomentController {
+
+    private final MomentService momentService;
+
+    @GetMapping("/{momentId}")
+    public ResponseEntity<MomentResponse.MomentDetail> getMomentDetail(@RequestHeader("Authorization") String authorization, @PathVariable Long momentId){
+        return new ResponseEntity<>(momentService.getMomentDetail(authorization,momentId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, Long>> createMoment(@RequestHeader("Authorization") String authorization, @RequestBody MomentRequest.CreateMomentInfo createMomentInfo){
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("momentId", momentService.createMoment(authorization, createMomentInfo));
+        return new ResponseEntity<Map<String, Long>>(resultMap, HttpStatus.OK);
+    }
+}
---
+++ Backend/src/main/java/com/sloth/meeplo/moment/service/MomentServiceImpl.java
@@ -0,0 +1,41 @@
+package com.sloth.meeplo.moment.service;
+
+import com.sloth.meeplo.global.exception.MeeploException;
+import com.sloth.meeplo.global.exception.code.CommonErrorCode;
+import com.sloth.meeplo.member.entity.Member;
+import com.sloth.meeplo.member.service.MemberService;
+import com.sloth.meeplo.moment.dto.request.MomentRequest;
+import com.sloth.meeplo.moment.dto.response.MomentResponse;
+import com.sloth.meeplo.moment.entity.Moment;
+import com.sloth.meeplo.moment.repository.MomentRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MomentServiceImpl implements MomentService{
+
+    private final MemberService memberService;
+
+    private final MomentRepository momentRepository;
+
+    @Override
+    public MomentResponse.MomentDetail getMomentDetail(String authorization, Long momentId) {
+        Member member = memberService.getMemberByAuthorization(authorization);
+        Moment moment = momentRepository.findById(momentId)
+                .orElseThrow(()-> new MeeploException(CommonErrorCode.NOT_EXIST_RESOURCE));
+
+        return MomentResponse.MomentDetail.builder().moment(moment).member(member).build();
+    }
+
+    // TODO: 2022-11-06 group과 연결된 schedule 생성 요소들 재확인필요 
+    @Override
+    @Transactional
+    public Long createMoment(String authorization, MomentRequest.CreateMomentInfo createMomentInfo) {
+//        Moment moment = momentRepository.save(Moment.)
+        return null;
+    }
+}
