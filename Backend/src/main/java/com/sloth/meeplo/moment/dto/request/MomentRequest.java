a46fda46b0b8dedce817cad16053d48823265467
rudwns06 <rudwns06@hanmail.net>
2022-11-07 00:24:22 +0900

    ⁉️ [FEAT]댓글남기기 api

--- Backend/src/main/java/com/sloth/meeplo/moment/dto/request/MomentRequest.java
+++ Backend/src/main/java/com/sloth/meeplo/moment/dto/request/MomentRequest.java
@@ -1,6 +1,9 @@
 package com.sloth.meeplo.moment.dto.request;
 
+import com.sloth.meeplo.moment.dto.response.MomentResponse;
+import com.sloth.meeplo.moment.entity.MomentComment;
 import com.sloth.meeplo.moment.type.MomentType;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -16,5 +19,23 @@ public class MomentRequest {
         private String photoUrl;
         private String content;
         private MomentType type;
+    }
+
+    @Getter
+    @ToString
+    @NoArgsConstructor
+    public static class CreateMomentCommentInfo{
+        private String comment;
+        private MomentRequest.MomentCommentLocation location;
+
+    }
+    @Getter
+    @ToString
+    @NoArgsConstructor
+    public static class MomentCommentLocation{
+        private Double xPoint;
+        private Double yPoint;
+        private Double angle;
+
     }
 }
