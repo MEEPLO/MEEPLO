a77dfea26b254f128b9112b6f45e1e7e621205b0
HannaJang <letnumdot124@gmail.com>
2022-11-07 22:37:19 +0900

    🪐 [UPDATE] add swipe & reverse momentPic feature

--- frontend/meeplo/src/components/moments/MomentPic.js
+++ frontend/meeplo/src/components/moments/MomentPic.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { View, Dimensions, Pressable, Modal, Animated } from 'react-native';
+import { View, Dimensions, Pressable, Modal, Animated, TouchableOpacity, Easing } from 'react-native';
 import styled from 'styled-components';
 import AutoHeightImage from 'react-native-auto-height-image';
 import { theme } from '../../assets/constant/DesignTheme';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons/faHeart';
 import { faComment } from '@fortawesome/free-solid-svg-icons/faComment';
-
-import MomentpicBack from './MomentPicBack';
 
 const MomentsCol = styled.View`
   height: ${({ height }) => height}px;
@@ -17,6 +15,15 @@ const MomentsCol = styled.View`
 
 const MomentPic = ({ uri, type, direction }) => {
   const [momentModal, setMomentModal] = React.useState(false);
+  const [touchStart, setTouchStart] = React.useState(0);
+  const [touchEnd, setTouchEnd] = React.useState(0);
+  const [imageFront, setImageFront] = React.useState(true);
+  const [imageUri, setImageUri] = React.useState();
+  const swipeAnim = React.useRef(new Animated.Value(0)).current;
+
+  React.useEffect(() => {
+    setImageUri(uri);
+  }, []);
 
   const windowWidth = Dimensions.get('window').width;
   const windowHeight = Dimensions.get('window').height;
@@ -32,6 +39,51 @@ const MomentPic = ({ uri, type, direction }) => {
     1: windowWidth * 0.8,
     2: windowWidth * 0.8,
     3: windowWidth * 0.5,
+  };
+  const uri1 = 'https://meeplo-bucket.s3.ap-northeast-2.amazonaws.com/ourmoment221107010049.png';
+  const uri2 = 'https://meeplo-bucket.s3.ap-northeast-2.amazonaws.com/ourmoment221107010141.png';
+
+  const rotateData = swipeAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
+
+  const setStartLocation = event => {
+    const { locationX, pageX } = event.nativeEvent;
+    setTouchStart(pageX);
+  };
+
+  const setEndLocation = event => {
+    const { locationY, pageY } = event.nativeEvent;
+    setTouchEnd(pageY);
+    console.log('out');
+    isSwiped();
+  };
+
+  const isSwiped = () => {
+    console.log('in&out', touchStart - touchEnd);
+    if (touchEnd - touchStart < 100) {
+      if (imageFront) {
+        setImageFront(false);
+        setTimeout(() => {
+          setImageUri(uri2);
+        }, 80);
+      } else {
+        setImageFront(true);
+        setTimeout(() => {
+          setImageUri(uri);
+        }, 80);
+      }
+      swipeAnim.setValue(0);
+      Animated.timing(swipeAnim, {
+        toValue: 1,
+        duration: 320,
+        easing: Easing.linear,
+        useNativeDriver: false,
+      }).start(() => {});
+      setTouchStart(0);
+      setTouchEnd(0);
+    }
   };
 
   return (
@@ -59,7 +111,19 @@ const MomentPic = ({ uri, type, direction }) => {
           <Pressable
             onPress={() => setMomentModal(false)}
             style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0 }}></Pressable>
-          <AutoHeightImage source={{ uri }} width={detailWidth[type]} />
+          {/* <AutoHeightImage source={{ uri }} width={detailWidth[type]} /> */}
+
+          <TouchableOpacity
+            onPressIn={setStartLocation}
+            onPressOut={setEndLocation}
+            style={{ width: imgWidth + 40, height: viewHeight[type], backgroundColor: 'transparent' }}>
+            <Animated.Image
+              style={{ width: imgWidth, height: viewHeight[type], transform: [{ rotateY: rotateData }] }}
+              resizeMode="contain"
+              source={{ uri: imageUri }}
+            />
+          </TouchableOpacity>
+
           <Pressable
             style={{
               width: 50,
