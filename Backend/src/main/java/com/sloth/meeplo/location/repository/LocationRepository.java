7276b2f8877ef850cc1b6abe59d11ceb66918bec
rudwns06 <rudwns06@hanmail.net>
2022-11-08 23:35:34 +0900

    ⁉️ [FEAT]haversine 알고리즘 적용 api 구현(검증필요)

--- Backend/src/main/java/com/sloth/meeplo/location/repository/LocationRepository.java
+++ Backend/src/main/java/com/sloth/meeplo/location/repository/LocationRepository.java
@@ -12,7 +12,7 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
     String HAVERSINE_FORMULA = "(6371 * acos(cos(radians(:latitude)) * cos(radians(l.lat)) *" +
             " cos(radians(l.lng) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(l.lat))))";
 
-    @Query("SELECT location FROM location l WHERE " + HAVERSINE_FORMULA + " < :distance ORDER BY "+ HAVERSINE_FORMULA + " DESC")
+    @Query("SELECT l FROM Location l WHERE " + HAVERSINE_FORMULA + " < :distance ORDER BY "+ HAVERSINE_FORMULA + " DESC")
     List<Location> findLocationsWithInDistance(@Param("latitude") double latitude, @Param("longitude") double longitude, @Param("distance") double distanceWithInKM);
 
 
