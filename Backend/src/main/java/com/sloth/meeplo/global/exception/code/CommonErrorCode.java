9236445dabe8be18583aac4fc8f5b1d39a6bc3e4
남궁준 <melon0soda@gmail.com>
2022-11-11 01:47:24 +0900

    :space_invader: [UPDATE] Refresh Token을 이용한 토큰 재발급 구현 - Access token이 만료되고 Refresh token이 유효하면 두 토큰 모두 재발급 - Refresh token이 만료되면 예외 발생

--- Backend/src/main/java/com/sloth/meeplo/global/exception/code/CommonErrorCode.java
+++ Backend/src/main/java/com/sloth/meeplo/global/exception/code/CommonErrorCode.java
@@ -12,7 +12,8 @@ public enum CommonErrorCode implements ErrorCode {
     HTTP_RESPONSE_ERROR(HttpStatus.REQUEST_TIMEOUT, "외부 API 호출 중 문제가 발생했습니다."),
     WRONG_TOKEN(HttpStatus.BAD_REQUEST, "잘못된 토큰입니다."),
     WRONG_URL(HttpStatus.BAD_REQUEST, "잘못된 URL입니다."),
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "없는 멤버입니다.");
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "없는 멤버입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "존재하지 않는 로그인 정보입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
