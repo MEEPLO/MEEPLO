95773c5a97c75a9ba29cf6ea60668a63da2bbec7
김혜림 <bljh1008@naver.com>
2022-11-13 14:46:36 +0900

    🌸[UPDATE] image files and build-settings

--- frontend/meeplo/src/assets/image/index.js
+++ frontend/meeplo/src/assets/image/index.js
@@ -6,7 +6,6 @@ const images = {
   meeploLogo: require('./meeploLogo.png'),
   kakaoLogin: require('./kakao_login_large_wide.png'),
   addIcon: require('./addIcon.png'),
-  testLoopy: require('./test_loopy.png'),
   frame: {
     polaroid: require('./polaroidExample.png'),
     dayFilm: require('./dayFilmExample.png'),
