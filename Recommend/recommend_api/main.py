cf1aeb07275f8d4934011c2ed555240b3de17654
minah741 <minah741@naver.com>
2022-11-13 23:46:56 +0900

    🍸 [FEAT] ADD center coordinate sample code

--- Recommend/recommend_api/main.py
+++ Recommend/recommend_api/main.py
@@ -3,6 +3,8 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+import dto, middlepoint
+
 app = FastAPI()
 
 
@@ -10,7 +12,6 @@ class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
-
 
 @app.get("/")
 def read_root():
@@ -25,3 +26,8 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.post("/meeplo/recommendation/v1")
+async def get_center_weight(coordinates: dto.CoordinateList):
+    return middlepoint.calc_center_weight()
+    # return coordinates.coordinates
