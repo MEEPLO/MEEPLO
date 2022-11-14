a26076574c4c141f895bc1f73cfc2ca5ca6c3e6a
신민아 <minah741@naver.com>
2022-11-14 14:02:09 +0900

    🍸 [FEAT] IMPL calculating center point api
    -  실제 들어온 값을 가지고 무게 중심을 찾는 api 구현 완료.

--- Recommend/recommend_api/main.py
+++ Recommend/recommend_api/main.py
@@ -1,33 +1,11 @@
-from typing import Union
-
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 import dto, middlepoint
 
 app = FastAPI()
 
+@app.post("/meeplo/recommendation/v1/center")
+def get_center_weight(coordinates: dto.CoordinateList):
+    return middlepoint.calc_center_weight(coordinates.coordinates)
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-@app.post("/meeplo/recommendation/v1")
-async def get_center_weight(coordinates: dto.CoordinateList):
-    return middlepoint.calc_center_weight()
-    # return coordinates.coordinates
+# @app.post("/meeplo/recommendation/v1/amuse")
