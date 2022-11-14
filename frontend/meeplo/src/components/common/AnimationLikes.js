ba4efff3203df79baf894cf7db7e6dc3f5fff333
HannaJang <letnumdot124@gmail.com>
2022-11-14 18:02:42 +0900

    🪐 [UPDATE] update modal filter

--- frontend/meeplo/src/components/common/AnimationLikes.js
+++ frontend/meeplo/src/components/common/AnimationLikes.js
@@ -5,9 +5,17 @@ const AnimationLikes = ({ isLiked }) => {
   const likeLottieRef = useRef();
 
   React.useEffect(() => {
-    isLiked ? likeLottieRef.current?.play(100, 181) : likeLottieRef.current?.play(0, 100);
+    isLiked ? likeLottieRef.current?.play(0, 100) : likeLottieRef.current?.play(100, 181);
   }, [isLiked]);
-  return <LottieView ref={likeLottieRef} source={require('../../assets/image/94181-like.json')} loop={false} autoPlay={false} />;
+
+  return (
+    <LottieView
+      ref={likeLottieRef}
+      source={require('../../assets/image/94181-like.json')}
+      loop={false}
+      autoPlay={false}
+    />
+  );
 };
 
 export default AnimationLikes;
