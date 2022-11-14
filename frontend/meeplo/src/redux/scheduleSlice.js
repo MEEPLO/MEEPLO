0f1451ec84a9b8434c08758031fd42338d8a8f61
김제관 <jekwan@kakao.com>
2022-11-14 16:13:05 +0900

    🐣 [FEAT] add create schdule post requst function

--- frontend/meeplo/src/redux/scheduleSlice.js
+++ frontend/meeplo/src/redux/scheduleSlice.js
@@ -3,6 +3,20 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createAsyncThunk, createSlice, isRejectedWithValue } from '@reduxjs/toolkit';
 import { MEEPLO_SERVER_BASE_URL } from '@env';
 
+export const createSchedule = createAsyncThunk('schedule/createSchedule', async schedule => {
+  try {
+    const accessToken = await AsyncStorage.getItem('@accessToken');
+    const response = await axios.post(`${MEEPLO_SERVER_BASE_URL}/schedule`, schedule, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return response.data;
+  } catch (err) {
+    return isRejectedWithValue(err.response.data);
+  }
+});
+
 const initialState = {
   // TODO: API 명세에 적혀있는대로 초기값 설정
   value: '초기약속',
@@ -11,9 +25,7 @@ const scheduleSlice = createSlice({
   name: 'schedule',
   initialState,
   reducers: {},
-  extraReducers: {
-    // [getSchedule.fulfilled]: (state, {payload}) => payload
-  },
+  extraReducers: {},
 });
 
 export default scheduleSlice.reducer;
