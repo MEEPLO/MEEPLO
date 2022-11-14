8426d3637e87c3a977f0accf864e424902d1bdf2
신민아 <minah741@naver.com>
2022-11-14 14:02:09 +0900

    🍸 [FEAT] IMPL calculating center point api
    -  실제 들어온 값을 가지고 무게 중심을 찾는 api 구현 완료.

--- Recommend/recommend_api/middlepoint.py
+++ Recommend/recommend_api/middlepoint.py
@@ -1,7 +1,28 @@
 from shapely.geometry import Polygon
+from typing import List
 
-def calc_center_weight():
+import dto
 
-    P = Polygon([[37.564097, 127.052362], [37.5654337, 127.043864], [37.5649304, 127.0524007]])
-    print(P.centroid)
-    return P.centroid.coords
+def calc_center_weight(coordinates: List[dto.Coordinate]):
+    if len(coordinates) == 1:
+        return coordinates[0]
+
+    if len(coordinates) == 2:
+        return {"lat": (coordinates[0].lat + coordinates[1].lat) / 2, "lng": (coordinates[0].lng + coordinates[1].lng) / 2}
+
+    rows = len(coordinates)
+    cols = 2
+    coordArray = [[0 for j in range(cols)] for i in range(rows)]
+
+    for i in range(rows):
+        coordArray[i][0] = coordinates[i].lat
+        coordArray[i][1] = coordinates[i].lng
+
+    P = Polygon(coordArray)
+
+    json = {
+        "lat": P.centroid.x,
+        "lng": P.centroid.y
+    }
+
+    return json
