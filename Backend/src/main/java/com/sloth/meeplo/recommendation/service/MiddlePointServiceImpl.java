a6191b39bab8b39b683f7363ebe57556d391987c
rudwns06 <rudwns06@hanmail.net>
2022-11-14 16:16:36 +0900

    ⁉️ [FEAT] external api

--- Backend/src/main/java/com/sloth/meeplo/recommendation/service/MiddlePointServiceImpl.java
+++ Backend/src/main/java/com/sloth/meeplo/recommendation/service/MiddlePointServiceImpl.java
@@ -2,15 +2,18 @@ package com.sloth.meeplo.recommendation.service;
 
 import com.sloth.meeplo.global.exception.MeeploException;
 import com.sloth.meeplo.global.exception.code.CommonErrorCode;
+import com.sloth.meeplo.global.util.ExternalAPIRequest;
 import com.sloth.meeplo.group.entity.Group;
 import com.sloth.meeplo.group.repository.GroupMemberRepository;
 import com.sloth.meeplo.group.service.GroupService;
 import com.sloth.meeplo.location.entity.Location;
 import com.sloth.meeplo.location.repository.LocationRepository;
 import com.sloth.meeplo.location.type.LocationType;
+import com.sloth.meeplo.member.dto.request.MemberRequest;
 import com.sloth.meeplo.member.entity.Member;
 import com.sloth.meeplo.member.repository.MemberRepository;
 import com.sloth.meeplo.member.service.MemberService;
+import com.sloth.meeplo.recommendation.dto.common.Coordinate;
 import com.sloth.meeplo.recommendation.dto.request.MiddlePointRequest;
 import com.sloth.meeplo.recommendation.dto.response.MiddlePointResponse;
 import com.sloth.meeplo.recommendation.dto.response.RouteMetaData;
@@ -18,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,6 +32,8 @@ public class MiddlePointServiceImpl implements MiddlePointService{
 
     private final GroupService groupService;
     private final MemberService memberService;
+
+    private final ExternalAPIRequest externalAPIRequest;
 
     @Override
     public MiddlePointResponse.StationList calcMiddleStations(String authorization, MiddlePointRequest.CoordinationList startData) {
@@ -64,11 +70,19 @@ public class MiddlePointServiceImpl implements MiddlePointService{
     }
 
     private String convertAddressFromCoordinate(double lat, double lng) {
-        return "강남구";
+        String response = externalAPIRequest.getKakaoAddressInfo(lng, lat);
+        return Arrays.stream(response.split(" "))
+                .filter(s -> s.endsWith("구"))
+                .findFirst()
+                .orElse("서울시");
     }
 
     private RouteMetaData calcDurationAndRoute(MiddlePointRequest.MemberStartLocation start, Location destination) {
         // TODO : openroutemap을 사용하여 시간과 경로를 얻어오기
+        // api 호출
+        externalAPIRequest.getTimeAndRouteInfo(
+                MemberRequest.ConvertedCoordinate.builder().lng(start.getLng()).lat(start.getLat()).build(),
+                MemberRequest.ConvertedCoordinate.builder().lat(destination.getLat()).lng(destination.getLng()).build());
         return RouteMetaData.builder()
                 .time(30)
                 .pointCoordinate(new ArrayList<>())
@@ -77,8 +91,14 @@ public class MiddlePointServiceImpl implements MiddlePointService{
 
     private List<Location> getMiddleStations(List<MiddlePointRequest.MemberStartLocation> coordinates) {
         // TODO : fastapi로 무게중심 좌표 찾아오기 -> graham scan 알고리즘을 이용하여 concave일 경우 외곽
+        // fastapi 호출
+
+
+        // graham scan
+
+
         double lat = 37.564820366666666;
         double lng = 127.04954223333333;
-        return locationRepository.findLocationsWithCoordination(lat, lng, 0.3, LocationType.AMUSE);
+        return locationRepository.findLocationsWithCoordination(lat, lng, 1, LocationType.STATION);
     }
 }
