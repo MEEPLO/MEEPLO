527e45f8639bfd00a23c422f32c24b83810501e5
rudwns06 <rudwns06@hanmail.net>
2022-11-16 16:53:27 +0900

    ⁉️ [REFACTOR] 모호하던 error code를 명확하게 변경

--- Backend/src/main/java/com/sloth/meeplo/moment/exception/code/MomentErrorCode.java
+++ Backend/src/main/java/com/sloth/meeplo/moment/exception/code/MomentErrorCode.java
@@ -13,7 +13,9 @@ public enum MomentErrorCode implements ErrorCode {
     NOT_YET_REACTED(HttpStatus.ACCEPTED, "표기되지 않은 상태입니다."),
     ALREADY_COMMENTED(HttpStatus.ACCEPTED, "이미 입력했습니다."),
     NO_MORE_DATA(HttpStatus.ACCEPTED, "더 이상 요소가 없습니다."),
-    NO_MOMENT_TYPE(HttpStatus.ACCEPTED, "없는 추억 타입입니다.");
+    NO_MOMENT_TYPE(HttpStatus.ACCEPTED, "없는 추억 타입입니다."),
+    NOT_EXIST_MOMENT(HttpStatus.ACCEPTED, "존재하지 않는 추억입니다."),
+    ;
     private final HttpStatus httpStatus;
     private final String message;
 }
