2ed142e172486bb26e53f624f7b46dc73eb0227b
jekwan <jekwan@kakao.com>
2022-11-18 01:57:17 +0900

    🐣 [UPDATE] fix round view structure

--- frontend/meeplo/src/components/common/ModalRound.js
+++ frontend/meeplo/src/components/common/ModalRound.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, Modal, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { Modal, StyleSheet } from 'react-native';
+
 import { theme } from '../../assets/constant/DesignTheme';
 
 import RoundView from './RoundView';
@@ -7,9 +8,24 @@ import RoundView from './RoundView';
 const ModalRound = ({ hideHeader, title, visible, onRequestClose, children }) => {
   return (
     <Modal animationType="fade" transparent={true} visible={visible} onRequestClose={onRequestClose}>
-      <RoundView hideHeade={hideHeader} title={title} onRequestClose={onRequestClose} chidlren={children} />
+      <View style={styles.centeredView}>
+        <RoundView hideHeade={hideHeader} title={title} onRequestClose={onRequestClose} children={children} />
+      </View>
     </Modal>
   );
 };
+
+const styles = StyleSheet.create({
+  centeredView: {
+    backgroundColor: theme.color.dim,
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default ModalRound;
