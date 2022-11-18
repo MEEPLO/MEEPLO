0683bfe27b442d1bfb2ec387328927e59c0f850a
신민아 <minah741@naver.com>
2022-11-18 23:21:20 +0900

    🍸 [FEAT] IMPL Keyword recommendation
    - w2v와 fast text를 이용하여 키워드를 뽑아내는 기능 구현 완료.
    
    
    Former-commit-id: 9d359c13c8c1a0955bbd952c28850cbb19077297 [formerly 6c0e202b16c62fe06893d9dc74282c3f5728d62d] [formerly dbae295649b14036353ee861915d4538b36681d3 [formerly 015810ff2401bf4d874a3535556d4820a26515bb]]
    Former-commit-id: afa8ebd320eb6b4a4124be1a9f60d883b3c38dd0 [formerly 52b4bbecf54fef15b8f2f792ac3ab7d2ab81ba34]
    Former-commit-id: 0d87f772308efdf23ec599ee1bf8440c68e724e6

--- Recommend/recommend_api/app/main.py
+++ Recommend/recommend_api/app/main.py
@@ -1,13 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 
-import dto, middlepoint, recommend
+# from fastapi import HTTPException, responses, Query
+from sqlalchemy.orm import Session
+from starlette.middleware.cors import CORSMiddleware
+
+import dto, middlepoint, recommend, model, database, schema
 
 app = FastAPI()
+
+model.Base.metadata.create_all(bind=database.engine)
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.post("/meeplo/recommendation/v1/center")
 def get_center_weight(coordinates: dto.CoordinateList):
     return middlepoint.calc_center_weight(coordinates.coordinates)
 
 @app.post("/meeplo/recommendation/v1/amuse")
-def get_keywords(tags: dto.TagList):
-    return recommend.recommend_keywords(tags.tags)
+async def get_keywords(
+    tags: dto.TagList, 
+    db: Session = Depends(get_db)
+):
+
+    json = {
+        "tags": recommend.recommend_keywords(tags.tags)
+    }
+
+    return json
