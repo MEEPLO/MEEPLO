16082a5a8714cab123b7fa9bb09e0997a4b3586a
jekwan <jekwan@kakao.com>
2022-11-19 22:05:05 +0900

    🐣 [FEAT] recommendation input and axiosPrivate

---
+++ frontend/meeplo/src/redux/recommendationSlice.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createAsyncThunk, createSlice, isRejectedWithValue } from '@reduxjs/toolkit';
+import { MEEPLO_SERVER_BASE_URL } from '@env';
+import { axiosPrivate } from '../auth/axiosInstance';
+
+export const getMiddlePoint = createAsyncThunk('recommendation/getMiddlePoint', async form => {
+  console.log(form);
+  try {
+    const accessToken = await AsyncStorage.getItem('@accessToken');
+    const response = await axiosPrivate.post(`/recommendation/location/middle`, form, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return response.data;
+  } catch (err) {
+    console.log('getMiddlePoint error', err.response.data);
+    return isRejectedWithValue(err.response.data);
+  }
+});
+
+const recommendationSlice = createSlice({
+  name: 'recommendation',
+  initialState: {
+    isLoading: false,
+    recommendedStations: {},
+  },
+  reducers: {},
+  extraReducers: {
+    [getMiddlePoint.pending]: (state, { payload }) => {
+      state.isLoading = true;
+    },
+    [getMiddlePoint.fulfilled]: (state, { payload }) => {
+      state.isLoading = false;
+      state.recommendedStations = payload?.stations;
+    },
+  },
+});
+
+export { recommendationSlice };
