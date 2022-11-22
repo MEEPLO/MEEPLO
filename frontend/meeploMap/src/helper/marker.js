7f82144991cf300a3fbdb1dbf3d2ec0a4e59ba94
김제관 <jekwan@kakao.com>
2022-11-22 16:38:59 +0900

    🐣 [UPDATE] map marker
    Former-commit-id: b4de3580759bfbdc87520a93026b23c0d61ef030 [formerly 7f5a04d32ead2b18130835d2727f571c9c87edbe] [formerly 59d22708bf6baeba5caafaaa380b2db0b325c350 [formerly 8812455418686530fe644262d71f43753019e234]]
    Former-commit-id: 20a4860eca89c163cb63c37a5dacea53b41c5e14 [formerly 3823f521cbbfda1850fa42a8ac429cd93b9f615d]
    Former-commit-id: 53077c7776565bc3758c7168c2ac17af51c1c28d

--- frontend/meeploMap/src/helper/marker.js
+++ frontend/meeploMap/src/helper/marker.js
@@ -1,70 +1,70 @@
-import MarkerNonePink from '../../assets/marker_none_pink.png';
-import MarkerNoneOrange from '../../assets/marker_none_orange.png';
-import MarkerNoneYellow from '../../assets/marker_none_yellow.png';
-import MarkerNoneGreen from '../../assets/marker_none_green.png';
-import MarkerNoneBlue from '../../assets/marker_none_blue.png';
-import MarkerNoneNavy from '../../assets/marker_none_navy.png';
-import MarkerNonePurple from '../../assets/marker_none_purple.png';
-import MarkerNoneRed from '../../assets/marker_none_red.png';
+import MarkerNoneAlert from "../../assets/marker_none_alert.png";
+import MarkerNoneOrange from "../../assets/marker_none_orange.png";
+import MarkerNoneYellow from "../../assets/marker_none_yellow.png";
+import MarkerNoneGreen from "../../assets/marker_none_green.png";
+import MarkerNoneBlue from "../../assets/marker_none_blue.png";
+import MarkerNoneNavy from "../../assets/marker_none_navy.png";
+import MarkerNonePurple from "../../assets/marker_none_purple.png";
+import MarkerNoneRed from "../../assets/marker_none_red.png";
 
-import MarkerStationPink from '../../assets/marker_station_pink.png';
-import MarkerStationOrange from '../../assets/marker_station_orange.png';
-import MarkerStationYellow from '../../assets/marker_station_yellow.png';
-import MarkerStationGreen from '../../assets/marker_station_green.png';
-import MarkerStationBlue from '../../assets/marker_station_blue.png';
-import MarkerStationNavy from '../../assets/marker_station_navy.png';
-import MarkerStationPurple from '../../assets/marker_station_purple.png';
-import MarkerStationRed from '../../assets/marker_station_red.png';
+import MarkerStationAlert from "../../assets/marker_station_alert.png";
+import MarkerStationOrange from "../../assets/marker_station_orange.png";
+import MarkerStationYellow from "../../assets/marker_station_yellow.png";
+import MarkerStationGreen from "../../assets/marker_station_green.png";
+import MarkerStationBlue from "../../assets/marker_station_blue.png";
+import MarkerStationNavy from "../../assets/marker_station_navy.png";
+import MarkerStationPurple from "../../assets/marker_station_purple.png";
+import MarkerStationRed from "../../assets/marker_station_red.png";
 
-import MarkerStorePink from '../../assets/marker_store_pink.png';
-import MarkerStoreOrange from '../../assets/marker_store_orange.png';
-import MarkerStoreYellow from '../../assets/marker_store_yellow.png';
-import MarkerStoreGreen from '../../assets/marker_store_green.png';
-import MarkerStoreBlue from '../../assets/marker_store_blue.png';
-import MarkerStoreNavy from '../../assets/marker_store_navy.png';
-import MarkerStorePurple from '../../assets/marker_store_purple.png';
-import MarkerStoreRed from '../../assets/marker_store_red.png';
+import MarkerStoreAlert from "../../assets/marker_store_alert.png";
+import MarkerStoreOrange from "../../assets/marker_store_orange.png";
+import MarkerStoreYellow from "../../assets/marker_store_yellow.png";
+import MarkerStoreGreen from "../../assets/marker_store_green.png";
+import MarkerStoreBlue from "../../assets/marker_store_blue.png";
+import MarkerStoreNavy from "../../assets/marker_store_navy.png";
+import MarkerStorePurple from "../../assets/marker_store_purple.png";
+import MarkerStoreRed from "../../assets/marker_store_red.png";
 
-import MarkerUserPink from '../../assets/marker_user_pink.png';
-import MarkerUserOrange from '../../assets/marker_user_orange.png';
-import MarkerUserYellow from '../../assets/marker_user_yellow.png';
-import MarkerUserGreen from '../../assets/marker_user_green.png';
-import MarkerUserBlue from '../../assets/marker_user_blue.png';
-import MarkerUserNavy from '../../assets/marker_user_navy.png';
-import MarkerUserPurple from '../../assets/marker_user_purple.png';
-import MarkerUserRed from '../../assets/marker_user_red.png';
+import MarkerUserAlert from "../../assets/marker_user_alert.png";
+import MarkerUserOrange from "../../assets/marker_user_orange.png";
+import MarkerUserYellow from "../../assets/marker_user_yellow.png";
+import MarkerUserGreen from "../../assets/marker_user_green.png";
+import MarkerUserBlue from "../../assets/marker_user_blue.png";
+import MarkerUserNavy from "../../assets/marker_user_navy.png";
+import MarkerUserPurple from "../../assets/marker_user_purple.png";
+import MarkerUserRed from "../../assets/marker_user_red.png";
 
-import MarkerHerePink from '../../assets/marker_here_pink.png';
-import MarkerHereOrange from '../../assets/marker_here_orange.png';
-import MarkerHereYellow from '../../assets/marker_here_yellow.png';
-import MarkerHereGreen from '../../assets/marker_here_green.png';
-import MarkerHereBlue from '../../assets/marker_here_blue.png';
-import MarkerHereNavy from '../../assets/marker_here_navy.png';
-import MarkerHerePurple from '../../assets/marker_here_purple.png';
-import MarkerHereRed from '../../assets/marker_here_red.png';
+import MarkerHereAlert from "../../assets/marker_here_alert.png";
+import MarkerHereOrange from "../../assets/marker_here_orange.png";
+import MarkerHereYellow from "../../assets/marker_here_yellow.png";
+import MarkerHereGreen from "../../assets/marker_here_green.png";
+import MarkerHereBlue from "../../assets/marker_here_blue.png";
+import MarkerHereNavy from "../../assets/marker_here_navy.png";
+import MarkerHerePurple from "../../assets/marker_here_purple.png";
+import MarkerHereRed from "../../assets/marker_here_red.png";
 
 export const MARKER_TYPE = {
-  NONE: 'none',
-  STATION: 'station',
-  STORE: 'store',
-  USER: 'user',
-  HERE: 'here',
+  NONE: "none",
+  STATION: "station",
+  STORE: "store",
+  USER: "user",
+  HERE: "here",
 };
 
 export const MARKER_COLOR = {
-  PINK: 'pink',
-  ORANGE: 'orange',
-  YELLOW: 'yellow',
-  GREEN: 'green',
-  BLUE: 'blue',
-  NAVY: 'navy',
-  PURPLE: 'purple',
-  RED: 'red',
+  ALERT: "alert",
+  ORANGE: "orange",
+  YELLOW: "yellow",
+  GREEN: "green",
+  BLUE: "blue",
+  NAVY: "navy",
+  PURPLE: "purple",
+  RED: "red",
 };
 
 const markers = {
   [MARKER_TYPE.NONE]: {
-    [MARKER_COLOR.PINK]: MarkerNonePink,
+    [MARKER_COLOR.ALERT]: MarkerNoneAlert,
     [MARKER_COLOR.ORANGE]: MarkerNoneOrange,
     [MARKER_COLOR.YELLOW]: MarkerNoneYellow,
     [MARKER_COLOR.GREEN]: MarkerNoneGreen,
@@ -74,7 +74,7 @@ const markers = {
     [MARKER_COLOR.RED]: MarkerNoneRed,
   },
   [MARKER_TYPE.STATION]: {
-    [MARKER_COLOR.PINK]: MarkerStationPink,
+    [MARKER_COLOR.ALERT]: MarkerStationAlert,
     [MARKER_COLOR.ORANGE]: MarkerStationOrange,
     [MARKER_COLOR.YELLOW]: MarkerStationYellow,
     [MARKER_COLOR.GREEN]: MarkerStationGreen,
@@ -84,7 +84,7 @@ const markers = {
     [MARKER_COLOR.RED]: MarkerStationRed,
   },
   [MARKER_TYPE.STORE]: {
-    [MARKER_COLOR.PINK]: MarkerStorePink,
+    [MARKER_COLOR.ALERT]: MarkerStoreAlert,
     [MARKER_COLOR.ORANGE]: MarkerStoreOrange,
     [MARKER_COLOR.YELLOW]: MarkerStoreYellow,
     [MARKER_COLOR.GREEN]: MarkerStoreGreen,
@@ -94,7 +94,7 @@ const markers = {
     [MARKER_COLOR.RED]: MarkerStoreRed,
   },
   [MARKER_TYPE.USER]: {
-    [MARKER_COLOR.PINK]: MarkerUserPink,
+    [MARKER_COLOR.ALERT]: MarkerUserAlert,
     [MARKER_COLOR.ORANGE]: MarkerUserOrange,
     [MARKER_COLOR.YELLOW]: MarkerUserYellow,
     [MARKER_COLOR.GREEN]: MarkerUserGreen,
@@ -104,7 +104,7 @@ const markers = {
     [MARKER_COLOR.RED]: MarkerUserRed,
   },
   [MARKER_TYPE.HERE]: {
-    [MARKER_COLOR.PINK]: MarkerHerePink,
+    [MARKER_COLOR.ALERT]: MarkerHereAlert,
     [MARKER_COLOR.ORANGE]: MarkerHereOrange,
     [MARKER_COLOR.YELLOW]: MarkerHereYellow,
     [MARKER_COLOR.GREEN]: MarkerHereGreen,
